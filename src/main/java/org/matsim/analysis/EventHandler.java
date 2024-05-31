5c0be50f4a0c6f089630325329139ba4c0341045
lion.pfeil <lionpfeil@gmail.com>
2024-05-31 22:46:32 +0200

    my first event handler

--- src/main/java/org/matsim/analysis/EventHandler.java
+++ src/main/java/org/matsim/analysis/EventHandler.java
@@ -1,19 +1,33 @@
 package org.matsim.analysis;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.events.PersonArrivalEvent;
 import org.matsim.api.core.v01.events.PersonDepartureEvent;
 import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.api.internal.HasPersonId;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EventHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler {
-    @Override
-    public void handleEvent(PersonArrivalEvent personArrivalEvent) {
 
-        System.out.println("Arrival:" + personArrivalEvent.getTime() + " " + personArrivalEvent.getPersonId());
-    }
+    private final Map<Id<Person>, Double> personToDepartureTime = new HashMap<>();
 
     @Override
     public void handleEvent(PersonDepartureEvent personDepartureEvent) {
-        System.out.println("Departure:" + personDepartureEvent.getTime() + " " + personDepartureEvent.getPersonId());
+        var departureTime = personDepartureEvent.getTime();
+        var personId = personDepartureEvent.getPersonId();
+        personToDepartureTime.put(personId, departureTime);
     }
+    @Override
+    public void handleEvent(PersonArrivalEvent personArrivalEvent) {
+        var arrivalTime = personArrivalEvent.getTime();
+        var departureTime = personToDepartureTime.get(personArrivalEvent.getPersonId());
+        var travelTime = arrivalTime - departureTime;
+
+        System.out.println("Person " + personArrivalEvent.getPersonId() + "arrives after " + travelTime + " seconds");
+    }
+
 }
